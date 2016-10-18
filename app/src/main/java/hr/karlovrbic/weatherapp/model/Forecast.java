8f463de9c4c8db90eaa629d278811358b8e9e62f
TheKarlo95 <karlo.vrbic@fer.hr>
2016-10-18 20:06:28 +0200

    Added prvate attirbute date

--- app/src/main/java/hr/karlovrbic/weatherapp/model/Forecast.java
+++ app/src/main/java/hr/karlovrbic/weatherapp/model/Forecast.java
@@ -1,19 +1,29 @@
 package hr.karlovrbic.weatherapp.model;
 
+import java.util.Date;
+
 /**
  * Created by TheKarlo95 on 17.10.2016..
  */
 public class Forecast {
 
     private City city;
+    private Date date;
     private Weather weather;
     private Temperature temperature;
     private Wind wind;
-    private Integer pressure;
+    private Double pressure;
     private Integer humidity;
 
-    public Forecast(City city, Weather weather, Temperature temperature, Wind wind, Integer pressure, Integer humidity) {
+    public Forecast(City city,
+                    Date date,
+                    Weather weather,
+                    Temperature temperature,
+                    Wind wind,
+                    Double pressure,
+                    Integer humidity) {
         this.city = city;
+        this.date = date;
         this.weather = weather;
         this.temperature = temperature;
         this.wind = wind;
