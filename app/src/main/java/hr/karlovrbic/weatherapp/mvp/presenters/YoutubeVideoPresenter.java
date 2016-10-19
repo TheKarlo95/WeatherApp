994feb61355971db3158043a28c3b9048ffb18f3
TheKarlo95 <karlo.vrbic@fer.hr>
2016-10-19 18:05:21 +0200

    Added presenter for the YoutubeVideoActivity

---
+++ app/src/main/java/hr/karlovrbic/weatherapp/mvp/presenters/YoutubeVideoPresenter.java
@@ -0,0 +1,44 @@
+package hr.karlovrbic.weatherapp.mvp.presenters;
+
+import hr.karlovrbic.weatherapp.mvp.interactors.YoutubeVideoInteractor;
+import hr.karlovrbic.weatherapp.mvp.interfaces.IYoutubeVideo;
+import hr.karlovrbic.weatherapp.network.ResponseListener;
+import hr.karlovrbic.weatherapp.utils.Objects;
+
+/**
+ * Created by TheKarlo95 on 19.10.2016..
+ */
+
+public class YoutubeVideoPresenter implements IYoutubeVideo.Presenter {
+
+    private IYoutubeVideo.View view;
+    private IYoutubeVideo.Interactor interactor;
+
+
+    public YoutubeVideoPresenter(IYoutubeVideo.View view) {
+        this.view = Objects.requireNonNull(view, "Parameter view cannnot be null");
+        this.interactor = new YoutubeVideoInteractor();
+    }
+
+    @Override
+    public void loadVideo(String keywords) {
+        view.showProgress();
+        interactor.getVideo(keywords, new ResponseListener<String>() {
+            @Override
+            public void onSuccess(String videoId) {
+                if(videoId != null) {
+                    view.cueVideo(videoId);
+                } else {
+                    view.showMessage("No videos found");
+                }
+                view.hideProgress();
+            }
+
+            @Override
+            public void onError(String message) {
+                view.showMessage(message);
+                view.hideProgress();
+            }
+        });
+    }
+}
