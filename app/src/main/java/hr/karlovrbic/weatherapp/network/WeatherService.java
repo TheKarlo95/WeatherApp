267f4563f92e29ef176fc4c91453b46ea23383af
TheKarlo95 <karlo.vrbic@fer.hr>
2016-10-19 10:09:29 +0200

    Added method for getting forecasts from OpenWeather API

---
+++ app/src/main/java/hr/karlovrbic/weatherapp/network/WeatherService.java
@@ -0,0 +1,17 @@
+package hr.karlovrbic.weatherapp.network;
+
+import hr.karlovrbic.weatherapp.model.response.ForecastsResponse;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
+/**
+ * Created by TheKarlo95 on 18.10.2016..
+ */
+public interface WeatherService {
+
+    @GET("forecast/daily?")
+    Observable<ForecastsResponse> getForecasts(@Query("q") String cityAndCountry,
+                                               @Query("cnt") String numOfDays,
+                                               @Query("APPID") String apiKey);
+}
