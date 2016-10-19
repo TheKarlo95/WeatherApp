83623e34725c1087fa71339ead1e1d702a36ee56
TheKarlo95 <karlo.vrbic@fer.hr>
2016-10-19 10:01:55 +0200

    Parcelable implementation

--- app/src/main/java/hr/karlovrbic/weatherapp/model/Wind.java
+++ app/src/main/java/hr/karlovrbic/weatherapp/model/Wind.java
@@ -1,30 +1,54 @@
 package hr.karlovrbic.weatherapp.model;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by TheKarlo95 on 17.10.2016..
  */
-public class Wind {
+public class Wind implements Parcelable {
 
-    @SerializedName("speed")
-    @Expose
+    public static final Parcelable.Creator<Wind> CREATOR = new Parcelable.Creator<Wind>() {
+        @Override
+        public Wind createFromParcel(Parcel source) {
+            return new Wind(source);
+        }
+
+        @Override
+        public Wind[] newArray(int size) {
+            return new Wind[size];
+        }
+    };
+
     private Double speed;
-    @SerializedName("deg")
-    @Expose
-    private Integer degrees;
+    private Double degrees;
 
-    public Wind(Double speed, Integer degrees) {
+    public Wind(Double speed, Double degrees) {
         this.speed = speed;
         this.degrees = degrees;
+    }
+
+    protected Wind(Parcel in) {
+        this.speed = (Double) in.readValue(Double.class.getClassLoader());
+        this.degrees = (Double) in.readValue(Double.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.speed);
+        dest.writeValue(this.degrees);
     }
 
     public Double getSpeed() {
         return speed;
     }
 
-    public Integer getDegrees() {
+    public Double getDegrees() {
         return degrees;
     }
 }
