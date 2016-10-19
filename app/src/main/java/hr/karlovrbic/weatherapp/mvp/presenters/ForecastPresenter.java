2f929dcb6323840a06f058590f34026038681643
TheKarlo95 <karlo.vrbic@fer.hr>
2016-10-19 16:46:36 +0200

    Added CurrentWeatherInteractor and implemented 'void showVideo(String, String)' method plus some minor changes to how forecasts are retrieved from the API

--- app/src/main/java/hr/karlovrbic/weatherapp/mvp/presenters/ForecastPresenter.java
+++ app/src/main/java/hr/karlovrbic/weatherapp/mvp/presenters/ForecastPresenter.java
@@ -7,6 +7,7 @@ import java.util.List;
 import hr.karlovrbic.weatherapp.model.City;
 import hr.karlovrbic.weatherapp.model.Country;
 import hr.karlovrbic.weatherapp.model.Forecast;
+import hr.karlovrbic.weatherapp.mvp.interactors.CurrentWeatherInteractor;
 import hr.karlovrbic.weatherapp.mvp.interactors.ForecastInteractor;
 import hr.karlovrbic.weatherapp.mvp.interfaces.IForecast;
 import hr.karlovrbic.weatherapp.network.ResponseListener;
@@ -21,21 +22,28 @@ public class ForecastPresenter implements IForecast.Presenter {
     @NonNull
     private IForecast.View view;
     @NonNull
-    private IForecast.Interactor interactor;
+    private IForecast.ForecastInteractor forecastInteractor;
+    @NonNull
+    private IForecast.CurrentWeatherInteractor currentWeatherInteractor;
+
+    private int callCounter;
 
     public ForecastPresenter(IForecast.View view) {
         this.view = Objects.requireNonNull(view, "Parameter view cannnot be null");
-        this.interactor = new ForecastInteractor();
+        this.forecastInteractor = new ForecastInteractor();
+        this.currentWeatherInteractor = new CurrentWeatherInteractor();
     }
 
     @Override
     public void showVideo(String cityName, String weatherDescription) {
-
+        String keywords = cityName + " " + weatherDescription;
+        view.startVideoActivity(keywords);
     }
 
     @Override
     public void getForecast(City city) {
         view.showProgress();
+        callCounter = 2;
 
         String cityName = null;
         String countryName = null;
@@ -47,11 +55,11 @@ public class ForecastPresenter implements IForecast.Presenter {
             }
         }
 
-        interactor.getForecast(cityName, countryName, new ResponseListener<List<Forecast>>() {
+        forecastInteractor.getForecast(cityName, countryName, new ResponseListener<List<Forecast>>() {
                     @Override
                     public void onSuccess(List<Forecast> result) {
                         view.setForecasts(result);
-                        view.hideProgress();
+                        hideProgress();
                     }
 
                     @Override
@@ -61,11 +69,31 @@ public class ForecastPresenter implements IForecast.Presenter {
                     }
                 }
         );
+        currentWeatherInteractor.getCurrentWeather(cityName, countryName, new ResponseListener<Forecast>() {
+            @Override
+            public void onSuccess(Forecast result) {
+                view.setCurrentWeather(result);
+                hideProgress();
+            }
+
+            @Override
+            public void onError(String message) {
+                view.showMessage(message);
+                view.hideProgress();
+            }
+        });
     }
 
     @Override
     public void cancel() {
-        interactor.cancel();
+        forecastInteractor.cancel();
         view.hideProgress();
+    }
+
+    private void hideProgress() {
+        callCounter--;
+        if (callCounter <= 0) {
+            view.hideProgress();
+        }
     }
 }
