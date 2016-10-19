203fde49d73d25861eed50077545250d2fb7ba38
TheKarlo95 <karlo.vrbic@fer.hr>
2016-10-19 18:06:12 +0200

    Replaced Youtube API key because 403 responses

--- app/src/main/java/hr/karlovrbic/weatherapp/Keys.java
+++ app/src/main/java/hr/karlovrbic/weatherapp/Keys.java
@@ -7,5 +7,5 @@ package hr.karlovrbic.weatherapp;
 public class Keys {
 
     public static final String OPEN_WEATHER_KEY = "a4b96343d0e4b132eb523d525fcd65d7";
-    public static final String YOUTUBE_API = "AIzaSyASPFSoPncWzvoNkT17Ex3qky2Jf2C4-_4";
+    public static final String YOUTUBE_API = "AIzaSyA92SuP5oldkj2nQA5DNBnJWzIrcT1wSWQ";
 }
