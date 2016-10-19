97cf6561c5964cddde36b61303fb74d564f1c526
TheKarlo95 <karlo.vrbic@fer.hr>
2016-10-19 10:01:48 +0200

    Parcelable implementation

--- app/src/main/java/hr/karlovrbic/weatherapp/model/Temperature.java
+++ app/src/main/java/hr/karlovrbic/weatherapp/model/Temperature.java
@@ -1,22 +1,67 @@
 package hr.karlovrbic.weatherapp.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by TheKarlo95 on 17.10.2016..
  */
 
-public class Temperature {
+public class Temperature implements Parcelable {
 
-    private Double currentTemperature;
-    private Double minTemperature;
-    private Double maxTemperature;
+    public static final Parcelable.Creator<Temperature> CREATOR = new Parcelable.Creator<Temperature>() {
+        @Override
+        public Temperature createFromParcel(Parcel source) {
+            return new Temperature(source);
+        }
 
-    public Temperature(Double minTemperature, Double maxTemperature, Double currentTemperature) {
-        this.currentTemperature = currentTemperature;
-        this.minTemperature = minTemperature;
-        this.maxTemperature = maxTemperature;
+        @Override
+        public Temperature[] newArray(int size) {
+            return new Temperature[size];
+        }
+    };
+
+    private Double current;
+    private Double min;
+    private Double max;
+
+    public Temperature(Double current, Double min, Double max) {
+        this.current = current;
+        this.min = min;
+        this.max = max;
     }
 
-    public Temperature(Double minTemperature, Double maxTemperature) {
-        this(minTemperature, maxTemperature, null);
+    protected Temperature(Parcel in) {
+        this.current = (Double) in.readValue(Double.class.getClassLoader());
+        this.min = (Double) in.readValue(Double.class.getClassLoader());
+        this.max = (Double) in.readValue(Double.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.current);
+        dest.writeValue(this.min);
+        dest.writeValue(this.max);
+    }
+
+    public Temperature(Double min, Double max) {
+        this(min, max, null);
+    }
+
+    public Double getCurrent() {
+        return current;
+    }
+
+    public Double getMin() {
+        return min;
+    }
+
+    public Double getMax() {
+        return max;
     }
 }
