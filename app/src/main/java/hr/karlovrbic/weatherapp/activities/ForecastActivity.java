3e8556375f48b9033f4e46a49f712178530d608c
TheKarlo95 <karlo.vrbic@fer.hr>
2016-10-19 16:39:32 +0200

    Now fully works with precise data

--- app/src/main/java/hr/karlovrbic/weatherapp/activities/ForecastActivity.java
+++ app/src/main/java/hr/karlovrbic/weatherapp/activities/ForecastActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,30 +19,33 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import hr.karlovrbic.weatherapp.R;
-import hr.karlovrbic.weatherapp.adapter.ForecastAdapter;
 import hr.karlovrbic.weatherapp.model.City;
 import hr.karlovrbic.weatherapp.model.Forecast;
-import hr.karlovrbic.weatherapp.model.Weather;
 import hr.karlovrbic.weatherapp.mvp.interfaces.IForecast;
 import hr.karlovrbic.weatherapp.mvp.presenters.ForecastPresenter;
+import hr.karlovrbic.weatherapp.recyclerview.adapter.ForecastAdapter;
 import hr.karlovrbic.weatherapp.utils.MessageUtils;
 
 public class ForecastActivity extends AppCompatActivity implements IForecast.View {
 
-    private static final String KEYWORDS_EXTRAS_KEY = "keywords";
+    public static final String KEYWORDS_EXTRAS_KEY = "keywords";
     private static final String FORECASTS_KEY = "forecasts";
 
     @BindView(R.id.forecast_toolbar)
     Toolbar toolbar;
     @BindView(R.id.forecast_rv_forecasts)
     RecyclerView rvForecasts;
+    @BindView(R.id.forecast_tv_city)
+    TextView tvCity;
 
     private ProgressDialog progressDialog;
     private IForecast.Presenter presenter;
 
     private City city;
-    private Weather weather;
     private ForecastAdapter adapter;
+
+    private Forecast currentWeather;// for today
+    private List<Forecast> forecasts;// for day 2 and 3
 
     public static Intent buildIntent(Context context) {
         return new Intent(context, ForecastActivity.class);
@@ -68,21 +72,50 @@ public class ForecastActivity extends AppCompatActivity implements IForecast.Vie
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         ArrayList<Forecast> forecasts = savedInstanceState.getParcelableArrayList(FORECASTS_KEY);
-        setForecasts(forecasts);
+        if (forecasts != null) {
+            city = forecasts.get(0).getCity();
+            tvCity.setText(city.toString());
+            loadRecyclerView(forecasts);
+        }
     }
 
     @Override
     @OnClick(R.id.forecast_fab)
     public void onShowVideoClick() {
-        if (city != null && weather != null) {
-            presenter.showVideo(city.getName(), weather.getDescription());
+        if (city != null && currentWeather != null) {
+            presenter.showVideo(city.getName(), currentWeather.getWeather().getDescription());
         }
     }
 
     @Override
     public void setForecasts(List<Forecast> forecasts) {
-        city = forecasts.get(0).getCity();
-        if(rvForecasts != null) {
+        this.forecasts = forecasts;
+        setForecasts();
+    }
+
+    @Override
+    public void setCurrentWeather(Forecast currentWeather) {
+        city = currentWeather.getCity();
+        tvCity.setText(city.toString());
+
+        this.currentWeather = currentWeather;
+        setForecasts();
+    }
+
+    private void setForecasts() {
+        if (currentWeather != null && forecasts != null && rvForecasts != null) {
+            currentWeather.getTemperature().setMax(forecasts.get(0).getTemperature().getMax());
+            currentWeather.getTemperature().setMin(forecasts.get(0).getTemperature().getMin());
+            List<Forecast> allForecasts = new ArrayList<>();
+            allForecasts.add(currentWeather);
+            allForecasts.addAll(forecasts.subList(1, forecasts.size()));
+
+            loadRecyclerView(forecasts);
+        }
+    }
+
+    private void loadRecyclerView(List<Forecast> forecasts) {
+        if (rvForecasts != null) {
             adapter = new ForecastAdapter(this, forecasts);
             rvForecasts.setLayoutManager(new LinearLayoutManager(this));
             rvForecasts.setAdapter(adapter);
@@ -94,7 +127,7 @@ public class ForecastActivity extends AppCompatActivity implements IForecast.Vie
         Bundle extras = new Bundle();
         extras.putString(KEYWORDS_EXTRAS_KEY, keywords);
 
-        Intent intent = ForecastActivity.buildIntent(this);
+        Intent intent = YoutubeVideoActivity.buildIntent(this);
         intent.putExtras(extras);
         startActivity(intent);
     }
@@ -149,8 +182,6 @@ public class ForecastActivity extends AppCompatActivity implements IForecast.Vie
             }
 
             presenter.getForecast(city);
-        } else {
-
         }
     }
 }
