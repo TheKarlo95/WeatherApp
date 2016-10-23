76e898286d8512e14224ae1d6d092e1e7c7a93cf
TheKarlo95 <karlo.vrbic@fer.hr>
2016-10-23 18:05:52 +0200

    Added some common methods to View and Presenter interface

--- app/src/main/java/hr/karlovrbic/weatherapp/mvp/interfaces/IBase.java
+++ app/src/main/java/hr/karlovrbic/weatherapp/mvp/interfaces/IBase.java
@@ -1,14 +1,24 @@
 package hr.karlovrbic.weatherapp.mvp.interfaces;
 
+import android.support.annotation.IdRes;
+
 /**
  * Created by TheKarlo95 on 17.10.2016..
  */
 public interface IBase {
 
     interface View {
+        void showMessage(@IdRes int resId);
+
+        void showMessage(String message);
+
+        void showProgress();
+
+        void hideProgress();
     }
 
     interface Presenter {
+        void cancel();
     }
 
     interface Interactor {
