945e44aaba20498798fe0ef291edf9f4c519f399
TheKarlo95 <karlo.vrbic@fer.hr>
2016-10-23 20:59:53 +0200

    Added functionality to create one AppComponent for dependency injection

--- app/src/main/java/hr/karlovrbic/weatherapp/WeatherApp.java
+++ app/src/main/java/hr/karlovrbic/weatherapp/WeatherApp.java
@@ -2,12 +2,16 @@ package hr.karlovrbic.weatherapp;
 
 import android.app.Application;
 
+import hr.karlovrbic.weatherapp.dagger.components.AppComponent;
+import hr.karlovrbic.weatherapp.dagger.components.DaggerAppComponent;
+
 /**
  * Created by TheKarlo95 on 23.10.2016..
  */
 public class WeatherApp extends Application {
 
     private static WeatherApp instance;
+    private AppComponent appComponent;
 
     public static WeatherApp get() {
         return instance;
@@ -17,5 +21,10 @@ public class WeatherApp extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        appComponent = DaggerAppComponent.create();
+    }
+
+    public AppComponent getAppComponent() {
+        return appComponent;
     }
 }
